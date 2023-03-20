8176c741a13816bd0922f36b92a19c7825c09245
Paul Morris <ptmorris03@gmail.com>
2023-03-20 13:24:18 -0400

    use typer cli

--- prepare_data.py
+++ prepare_data.py
@@ -3,35 +3,39 @@ from safetensors.torch import save_file
 import torch
 from tqdm import tqdm
 from transformers import BertTokenizer
+import typer
 
 from pathlib import Path
 
+def load_data():
+    dataset = load_dataset("wikitext", "wikitext-103-v1")
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    vocab_size = tokenizer.vocab_size
+    special_token = tokenizer.all_special_tokens
 
-CTX_LEN = 512
-data_folder = Path("data/")
+    print(vocab_size, special_token)
 
+    return dataset, tokenizer
 
-data_folder.mkdir(exist_ok=True)
+def prepare(dataset_name, data_folder, context_length, dataset, tokenizer):
 
-dataset = load_dataset("wikitext", "wikitext-103-v1")
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-vocab_size = tokenizer.vocab_size
-special_token = tokenizer.all_special_tokens
-
-print(vocab_size, special_token)
-
-def prepare(dataset_name):
     tokens = []
     texts = []
     for sample in tqdm(dataset[dataset_name], dataset_name):
         tokens.extend(tokenizer(sample['text'])['input_ids'])
-        if len(tokens) >= CTX_LEN:
-            save, tokens = tokens[:CTX_LEN], tokens[CTX_LEN:]
+        if len(tokens) >= context_length:
+            save, tokens = tokens[:context_length], tokens[context_length:]
             texts.append(save)
             
     texts = torch.tensor(texts)
     save_file({"text": texts}, Path(data_folder, F"{dataset_name}.safetensors"))
 
-prepare("test")
-prepare("validation")
-prepare("train")
+def main(data_folder: Path = "data/", context_length: int = 512):
+    data_folder.mkdir(exist_ok=True)
+    dataset, tokenizer = load_data()
+    prepare("test", data_folder, context_length, dataset, tokenizer)
+    prepare("validation", data_folder, context_length, dataset, tokenizer)
+    prepare("train", data_folder, context_length, dataset, tokenizer)
+
+if __name__ == "__main__":
+    typer.run(main)
